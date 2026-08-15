10467316056357789f8d5b50b627d822219f8c1a
okdo0509 <okdo0509@gmail.com>
2026-08-16 01:12:09 +0900

    Operation : fata viam invenient, gradient, loss funtion 구현

--- main.py
+++ main.py
@@ -1,79 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-clase_data = [[1,1],[2,1],[3,4],[6,5],[4,2],[12,25],[11,28],[15,30],[18,37],[19,35]]
-clase_target = [0,0,0,0,0,1,1,1,1,1]
+def f(num):
+    return 1/(1+np.exp(num))
+def diff_f_W(num):
+    return -f(num)*(1-f(num))
+def diff_m_w(y_i,x_i):
+    return y_i*(x_i/np.linalg.norm(weights)-(np.dot(weights,x_i)+bias)*weights/(np.linalg.norm(weights)**3))
+def diff_m_b(y_i,x_i):
+    return (y_i/np.linalg.norm(weights))
 
-weights = np.array([10,-3])
-bias = -45
-clase_data = np.array(clase_data)
-clase_target = np.array(clase_target)
-best_b = 0
-best_w0 = 0
-best_w1 = 0
-min_count = 10
-for nb in range(-10,11,1):
-    distances = []
-    answer = []
-    for data in clase_data:
-        result = (np.dot(weights, data) + bias+nb) / np.sqrt(np.sum((weights)**2))
-        distances.append(result)
-        if result > 0:
-            answer.append(1)
-        else:
-            answer.append(0)
-    count = np.sum(np.array(answer) != clase_target)
-    if count < min_count:
-        min_count = count
-        best_b = nb
+class_data = [[1,1],[2,1],[3,4],[6,5],[4,2],[12,25],[11,28],[15,30],[18,37],[19,35]]
+class_target = [0,0,0,0,0,1,1,1,1,1]
 
-min_count = 10
-for nw0 in range(-50,51,1):
-    weights = np.array([nw0,-3])
-    distances = []
-    answer = []
-    for data in clase_data:
-        result = (np.dot(weights, data) + bias) / np.sqrt(np.sum((weights)**2))
-        distances.append(result)
-        if result > 0:
-            answer.append(1)
-        else:
-            answer.append(0)
-    count = np.sum(np.array(answer) != clase_target)
-    if count < min_count:
-        min_count = count
-        best_nw0 = nw0
+weights = np.array([10.0,-3.0])
+bias = -45.0
+class_data = np.array(class_data)
+class_target = np.array(class_target)
+class_y = np.array([-1,-1,-1,-1,-1,1,1,1,1,1])
 
-min_count = 10
-for nw1 in range(-50,51,1):
-    weights = np.array([10,nw1])
-    distances = []
-    answer = []
-    for data in clase_data:
-        result = (np.dot(weights, data) + bias) / np.sqrt(np.sum((weights)**2))
-        distances.append(result)
-        if result > 0:
-            answer.append(1)
-        else:
-            answer.append(0)
-    count = np.sum(np.array(answer) != clase_target)
-    if count < min_count:
-        min_count = count
-        best_nw1 = nw1
-
-print(bias+best_b,best_nw0, best_nw1)
-bias = bias+best_b
-distances = []
-answer = []
-weights = np.array([best_nw0,best_nw1])
-for data in clase_data:
-    result = (np.dot(weights, data) + bias) / np.sqrt(np.sum((weights)**2))
-    distances.append(result)
-    if result > 0:
-        answer.append(1)
-    else:
-        answer.append(0)
-count = np.sum(np.array(answer) != clase_target)
-print(distances)
-print(count)
-
+for epoch in range(10):
+    n = 2
+    m = []
+    for i in range(len(class_data)):
+        m.append(class_y[i] * (np.dot(weights, class_data[i]) + bias) / np.linalg.norm(weights))
+    gradient_w = np.sum(np.array([diff_f_W(m[i]) * diff_m_w(class_y[i], class_data[i]) for i in range(len(class_data))]), axis=0)
+    gradient_b = np.sum(np.array([diff_f_W(m[i]) * diff_m_b(class_y[i], class_data[i]) for i in range(len(class_data))]), axis=0)
+    print(f"Epoch {epoch+1}/ loss {np.sum(np.array([f(m[i]) for i in range(len(m))]))/len(m)}")
+    weights -= n * gradient_w
+    bias -= n * gradient_b
